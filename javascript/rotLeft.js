a71585aa7075724e5bfe3a42de4c332d33829a64
Bob <maydaybob2000@gmail.com>
2020-07-13 18:02:42 +0800

    Create rotLeft.js

---
+++ javascript/rotLeft.js
@@ -0,0 +1,15 @@
+function rotLeft(a, d) {
+  // a = 1, 2, 3, 4, 5
+  // d = 4 left roataion
+  // result = 5, 1, 2, 3, 4
+  let result = []
+  let a_length = a.length
+  for (let i = 0; i < a_length; i++) {
+    let idx = i + d
+    if (idx >= a_length) {
+      idx = idx - a_length
+    }
+    result.push(a[idx])
+  }
+  return result
+}

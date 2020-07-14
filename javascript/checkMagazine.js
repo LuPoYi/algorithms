e51c7c67cdd7ea646999d36eedbeb470f352203b
Bob <maydaybob2000@gmail.com>
2020-07-14 16:55:55 +0800

    Create checkMagazine.js

---
+++ javascript/checkMagazine.js
@@ -0,0 +1,18 @@
+// Complete the checkMagazine function below.
+function checkMagazine(magazine, note) {
+  // give me one grand today night < magzine
+  // give one grand today < bad guy
+
+  let is_ok = true
+
+  for (let i = 0; i < note.length; i++) {
+    let index = magazine.indexOf(note[i])
+    magazine.splice(index, 1);
+    if (index == -1) {
+      is_ok = false;
+      break;
+    }
+  }
+
+  return is_ok ? "Yes" : "No"
+}
